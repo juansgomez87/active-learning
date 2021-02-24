645d5085ffbc3dffcbc13186746032bd92c9f3fc
Juan Sebastián Gómez <juans.gomez87@gmail.com>
2021-02-24 02:22:13 +0100

    updates settings

--- settings.py
+++ settings.py
@@ -16,7 +16,6 @@ else:
     path_models_users = './models/users'
     path_to_data = './data'
 
-
 # trompa data
 path_to_audio = '{}/audio'.format(path_to_data)
 path_to_feats = '{}/feats'.format(path_to_data)
